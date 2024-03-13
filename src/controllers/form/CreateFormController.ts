d11fb174250f0b11fe36ee85167ba8c6aeb53d56
Gustavo Farias <gustavo.silva@ccc.ufcg.edu.br>
2024-03-13 13:52:41 -0300

    Update createFormController

--- src/controllers/form/CreateFormController.ts
+++ src/controllers/form/CreateFormController.ts
@@ -3,9 +3,10 @@ import { CreateFormService } from '../../services/form/CreateFormService';
 
 class CreateFormController {
     async handle(req: Request, res: Response) {
+        const order_id = req.headers.order_id as string;
         const { data } = req.body;
         const createFormService = new CreateFormService;
-        const form = await createFormService.execute({ data });
+        const form = await createFormService.execute({ order_id, data });
         return res.json(form);
     };
 };
